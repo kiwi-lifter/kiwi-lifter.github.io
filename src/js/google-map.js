047854993dabd0065220ae335fa44c6b10d4b872
kiwi-lifter <kiwilifter808@gmail.com>
2016-09-14 23:58:46 +1200

    Finally got the Yelp api cooperating with successful GETs.

--- src/js/google-map.js
+++ src/js/google-map.js
@@ -1,15 +1,35 @@
+
+
 var map;
+var locations;
 
    function initMap(data){
-		
-		
+	   
+	    // map style arrays
+  var styles = [{"featureType":"all","elementType":"all","stylers":[{"saturation":-100},{"gamma":0.5}]}] // StyleMapType object 
+  var styledMap = new google.maps.StyledMapType(styles,
+    {name: "Restaurants"});
+	
+  // Map object
+  var mapOptions = {
+    mapTypeControlOptions: {
+      mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'map_style']
+    }
+  };
+  
+		map = new google.maps.Map(document.getElementById('map'), 
+			 mapOptions);
+	/**
 		map = new google.maps.Map(document.getElementById('map'), {
 			center: {lat: -36.854222, lng: 174.745765},
 			zoom: 14
-		});
+		}, mapOptions);
 		
-		
-		var locations = data.restaurants;
+	**/	
+		map.mapTypes.set('map_style', styledMap);
+		map.setMapTypeId('map_style');
+  
+		locations = data.restaurants;
 		
 		var largeInfowindow = new google.maps.InfoWindow();
 		
@@ -20,13 +40,25 @@ var map;
           // Get the position from the location array.
           var position = locations[i].location;
           var title = locations[i].name;
+		  
+		  var test = getYelpInfo(locations[i]);
+		  
+		  console.log('>>>>>>>>>>>', test);
+		  
+		  // marker is an object with additional data about the pin for a single location
+    var markerImage = {
+		url: 'images/marker-orange.png',
+		scaledSize: new google.maps.Size(35, 35), // scaled size
+	};
+		  
           // Create a marker per location, and put into markers array.
           var marker = new google.maps.Marker({
             map: map,
             position: position,
             title: title,
             animation: google.maps.Animation.DROP,
-            id: i
+            id: i,
+			icon: markerImage
           });
           // Add the marker to locations array.
 		  locations[i].marker = marker;
@@ -49,11 +81,12 @@ var map;
     // one infowindow which will open at the marker that is clicked, and populate based
     // on that markers position.
     function populateInfoWindow(marker, infowindow) {
+		
         // Check to make sure the infowindow is not already opened on this marker.
 		
         if (infowindow.marker != marker) {
           infowindow.marker = marker;
-          infowindow.setContent('<div>' + marker.title + '</div>');
+          infowindow.setContent('<div>' + marker.title+'</div>');
           infowindow.open(map, marker);
 		  
           // Make sure the marker property is cleared if the infowindow is closed.
@@ -79,59 +112,25 @@ var map;
 			}
 			
 	}
-	
-	
-	
-	
-//(function($){	
-
-
-function nonce_generate() {
-    return (Math.floor(Math.random() * 1e12).toString());
-}
-
-
-var yelp_url = 'http://api.yelp.com/v2/search/?location=Auckland+New+Zealand';
-  var parameters = {
-      oauth_consumer_key: '_2Ah0MhfqD-6bTRas90k-g',
-      oauth_token: 'iPD6CD5VbxITVXljAxqNFbksMf7f4z6C',
-      oauth_nonce: nonce_generate(),
-      oauth_timestamp: Math.floor(Date.now() / 1000),
-      oauth_signature_method: 'HMAC-SHA1',
-      oauth_version: '1.0',
-      callback: 'cb'
-  };
-  var encodedSignature = oauthSignature.generate('GET', yelp_url, parameters, '4tvw4xVA9rF-ERUfxIO35I6E0Lo', 'GaSqIVTr_yhTgPTnabngJCvOAy8');
-  parameters.oauth_signature = encodedSignature;
-  var settings = {
-      url: yelp_url,
-      data: parameters,
-      cache: true,
-      dataType: 'jsonp',
-      success: function(results) {
-        console.log(results);
-      },
-      error: function() {
-		  
-        console.log('The Yelp API call failed :(');
 		
-		console.log('parameters', parameters);
-      }
-  };
-  $.ajax(settings);
+		
 
-/**
 function generateNonce() {
 	return (Math.floor(Math.random() * 1e12).toString());
 }
 
-var YELP_BASE_URL = 'http://api.yelp.com/v2/search?term=food&location='
+function getYelpInfo(target){
+
+//var YELP_BASE_URL = 'http://api.yelp.com/v2/search/'
+var YELP_BASE_URL = ''+ target.web +''
 	,YELP_CONSUMER_KEY = '_2Ah0MhfqD-6bTRas90k-g'
 	,YELP_TOKEN = 'iPD6CD5VbxITVXljAxqNFbksMf7f4z6C'
 	,YELP_CONSUMER_SECRET = '4tvw4xVA9rF-ERUfxIO35I6E0Lo' 
 	,YELP_TOKEN_SECRET = 'GaSqIVTr_yhTgPTnabngJCvOAy8';  
 	
-var yelpURL = YELP_BASE_URL + 'Ponsonby+Auckland';
+//var yelpURL = YELP_BASE_URL + 'Ponsonby+Auckland';
+
+var yelpURL = YELP_BASE_URL;
 
 var parameters = {
 	oauth_consumer_key: YELP_CONSUMER_KEY,
@@ -140,10 +139,17 @@ var parameters = {
 	oauth_timestamp: Math.floor(Date.now()/1000),
 	oauth_signature_method: 'HMAC-SHA1',
 	oauth_version : '1.0',
-	callback: 'cb'              // This is crucial to include for jsonp implementation in 
-                               // AJAX or else the oauth-signature will be wrong.
-};
+	callback: 'cb',
 
+	
+	//term: '262 Ponsonby Rd, Ponsonby, Auckland, NZ',
+	//term: target.address,
+	location: yelpURL,
+	//location: 'Ponsonby+Auckland',
+	limit: 20                   
+	};
+	
+	
 
 var encodedSignature = oauthSignature.generate('GET',yelpURL, parameters, YELP_CONSUMER_SECRET, YELP_TOKEN_SECRET);
 parameters.oauth_signature = encodedSignature;
@@ -157,7 +163,12 @@ var settings = {
 		dataType: 'jsonp',
 		success: function(results) {
 		// Do stuff with results
+		
 		console.log(results);
+		
+		//target.img = results.image_url;
+		
+		return results;
 		},
 		fail: function() {
 			// Do stuff on fail
@@ -167,44 +178,5 @@ var settings = {
  
 
 		// Send AJAX request via jQuery library
-		var test = $.ajax(settings);
-
-
-**/
-
-/**
-// Request API access: http://www.yelp.com/developers/getting_started/api_access
-var Yelp = require('yelp');
-
-var yelp = new Yelp({
-  consumer_key: '_2Ah0MhfqD-6bTRas90k-g',
-  consumer_secret: '4tvw4xVA9rF-ERUfxIO35I6E0Lo',
-  token: 'BtWHvXNuE1nUP0afRuHL2-e6TJT90yAB',
-  token_secret: 'Ef8tveoD40HEEIA4hHA0F5ihgjc',
-});
-
-// See http://www.yelp.com/developers/documentation/v2/search_api
-yelp.search({ term: 'food', location: 'Auckland' })
-.then(function (data) {
-  console.log(data);
-})
-.catch(function (err) {
-  console.error(err);
-});
-
-// See http://www.yelp.com/developers/documentation/v2/business
-yelp.business('yelp-san-francisco')
-  .then(console.log)
-  .catch(console.error);
-
-yelp.phoneSearch({ phone: '+15555555555' })
-  .then(console.log)
-  .catch(console.error);
-
-// A callback based API is also available:
-yelp.business('yelp-san-francisco', function(err, data) {
-  if (err) return console.log(error);
-  console.log(data);
-});	
-	**/
-//}(jQuery));
+		$.ajax(settings);
+};
